c6d986fd7497a3682b17c31a7fea9ac81a146445
Sean Lubner <seanlubner@gmail.com>
2013-09-18 03:42:01 -0700

    parsing of general results from wolfram alpha

--- HW2/CalCalc.py
+++ HW2/CalCalc.py
@@ -6,13 +6,16 @@ def calculate(command_string, use_wolfram=False):
         query_address = 'http://api.wolframalpha.com/v2/query?input=' + \
         urllib2.quote(wolf_string).replace('/','%2F') + \
         '&appid=UAGAWR-3X6Y8W777Q'
-        print 'talking to Wolfram Alpha...\n\t'
+        print '\nTalking to Wolfram Alpha, please wait...\n'
         search_data = urllib2.urlopen(query_address).read()
-        search_start = search_data.find("<pod title='Input")
-        search_start += search_data[search_start+1:].find("<pod title=") # next pod is the result
-        result_start = search_data[search_start:].find("<plaintext>")+search_start+11
-        result_end = search_data[search_start:].find("</plaintext>")+search_start
-        print 'From Wolfram Alpha:', search_data[result_start:result_end]
+        if "<queryresult success='true'" in search_data: # search was successful
+            search_start = search_data.find("<pod title='Input")
+            search_start += search_data[search_start+1:].find("<pod title=") # next pod is the result
+            result_start = search_data[search_start:].find("<plaintext>")+search_start+11
+            result_end = search_data[search_start:].find("</plaintext>")+search_start
+            print 'From Wolfram Alpha:', search_data[result_start:result_end]
+        else:
+            print "Wolfram Alpha did not understand your question."
         
             
     is_dangerous = ('os.' in command_string) or ('__' in command_string)
@@ -25,6 +28,7 @@ def calculate(command_string, use_wolfram=False):
                 result = eval(str(command_string))
                 print result
             except:
+                print "I don't know the answer to this question, so let's try Wolfram Alpha."
                 wolf_search(str(command_string))
                 
         else: # use the interwebz
@@ -35,7 +39,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CalCalc Application')
     parser.add_argument('-s', action='store', dest='command_string',
                         help='This is the command string for your query.')
-    parser.add_argument('-t', action='store_true', default=False,
+    parser.add_argument('-W', action='store_true', default=False,
                         dest='use_wolfram_online',
                         help='Apply this flag if you wish to search on Wolfram Alpha.')
     results = parser.parse_args()
