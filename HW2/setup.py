44d530bcd662693425ed3e4a0ef80bacce2b9663
Sean Lubner <seanlubner@gmail.com>
2013-09-18 13:53:46 -0700

    populating setup.py file

--- HW2/setup.py
+++ HW2/setup.py
@@ -1,4 +1,9 @@
 from distutils2.core import setup
+
 setup(name='CalCalc',
       version='1.0',
+      summary='CalCalc Querier for AY250 HW2',
+      author='Sean Lubner',
+      author_email='seanlubner@gmail.com',
+      home_page='https://github.com/Phononic/Sean_Lubner_250HWs',
       py_modules=['CalCalc'])
