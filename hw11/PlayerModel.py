e3d0a63de3f69b3de991bf03cc11cacdaa389f64
Sean Lubner <seanlubner@gmail.com>
2013-11-21 04:27:00 -0800

    Parts a through c comlete

--- hw11/PlayerModel.py
+++ hw11/PlayerModel.py
@@ -17,21 +17,25 @@ import pandas as pd
 data = pd.read_table('laa_2011_April.txt')
 N = data['AB'] # number of at-bats for each player
 num_hits = data['H'] # number of successful hits for each player
-i=1 # which player?
 
 # Prior
 """ mu (Beta Function as conjugate prior to Binomial distribution liklihood)
     In 2010 the mean batting average was 0.255 and the variance between players was 0.0011.
     Using the values above and solving the two above equations: alpha=43.78, beta=127.92 """
-mu = pymc.Beta('mu', alpha=43.78, beta=127.92)
+mus = []
+for i in np.arange(len(N)):
+    mus.append(pymc.Beta('mu'+str(i), alpha=43.78, beta=127.92))
 
 # Model
 """ Trivial model in this case """
 @pymc.deterministic(plot=False)
-def p_model(mu=mu):
+def p_model(mus=mus):
     """ Simple model; just return the expected average """
-    return mu
+    return mus
 
 # Likelihood
 """ A player either hits or misses => Binomial distribution """
-xi = pymc.Binomial('xi', n=N[i], p=p_model, value=num_hits[i], observed=True)
+xs = []
+for i in np.arange(len(N)):
+    xs.append(pymc.Binomial('x'+str(i), n=N[i], p=p_model[i],
+                                   value=num_hits[i], observed=True))
--- hw11/README.txt
+++ hw11/README.txt
@@ -7,3 +7,9 @@ Note: the data files being used are modified to remove a "double tab" between th
 The MLE is simply the success rate for a Bernoulli experiment.  Therefore, the number of successful hits, divided by the number of at bats, gives the MLE for each player.  This quantity is conveniently already calculated in the "AVG" (average) column of the data.
 
 ---------------- Problem (b) ----------------
+A Beta function prior with mean 0.255 and variance 0.0011 takes as hyperparamters, alpha = 43.78, and beta = 127.92.  The model (PlayerModel.py) uses this Beta function as the prior distribution on the batting average, mu.  The liklihood is a binomial distribution, because a batting average is computed from Bernoulli experiments (hit or a miss).  The model is a simple model, because we are just trying to estimate a single number (the batting average) rather than an entire function.
+
+---------------- Problem (c) ----------------
+Convergence was confirmed by looking both at the traces of all mus, as well as the z-score plots for each mu.  They do converge.
+
+---------------- Problem (d) ----------------
